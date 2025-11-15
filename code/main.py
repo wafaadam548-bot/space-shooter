af6245d7193ec56f1e8832531f5d84a3bedba2bd
Wafa Adam <wafaadam548@gmail.com>
2025-11-15 06:40:08 -0500

    add the screen for a long time

--- code/main.py
+++ code/main.py
@@ -1,10 +1,12 @@
 import pygame
-Window_Width,Window_Height=1200,700
+Window_Width,Window_Height=1000, 600
 pygame.init()
 disPlay_surfec=pygame.display.set_mode((Window_Width,Window_Height))
 running=True
 while running:
     for event in pygame.event.get():
-        if event.type==quit:
+        if event.type==pygame.QUIT:
             running=False
+    pygame.display.update()
+pygame.quit()            
 
