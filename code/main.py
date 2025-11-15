1fa99c8d40c57c93169fd67e09a67c924b266325
Wafa Adam <wafaadam548@gmail.com>
2025-11-15 06:30:07 -0500

    add that the player can close the windoes

--- code/main.py
+++ code/main.py
@@ -1,4 +1,10 @@
 import pygame
-Window_Width,Window_Height=1280,720
+Window_Width,Window_Height=1200,700
 pygame.init()
 disPlay_surfec=pygame.display.set_mode((Window_Width,Window_Height))
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==quit:
+            running=False
+
