375f4b4364dbc278a94cde31d3da6d4b851095c8
Wafa Adam <wafaadam548@gmail.com>
2025-11-15 07:26:21 -0500

    change the background and ,ake a rect

--- code/main.py
+++ code/main.py
@@ -4,11 +4,13 @@ pygame.init()
 disPlay_surfec=pygame.display.set_mode((Window_Width,Window_Height))
 pygame.display.set_caption("Space Shoter")
 running=True
+surf=pygame.Surface((100,200))
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-    disPlay_surfec.fill("blue")        
+    disPlay_surfec.fill("darkGray")    
+    disPlay_surfec.blit(surf,(100,150))    
     pygame.display.update()
 pygame.quit()            
 
