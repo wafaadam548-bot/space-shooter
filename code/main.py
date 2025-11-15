f4345cc1a997a2235e24ee4d1703e6788e45428a
Wafa Adam <wafaadam548@gmail.com>
2025-11-15 08:11:20 -0500

    andd randome stars

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 Window_Width,Window_Height=1000, 600
 pygame.init()
 disPlay_surfec=pygame.display.set_mode((Window_Width,Window_Height))
@@ -7,13 +8,15 @@ running=True
 surf=pygame.Surface((100,200))
 surf.fill("orange")
 x=100
-player=pygame.image.load("images/player.png")
+star=pygame.image.load("images/star.png").convert_alpha()
+player=pygame.image.load("images/player.png").convert_alpha()#convert alpha like collisionshape in godot
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
     disPlay_surfec.fill("darkGray")    
-    disPlay_surfec.blit(surf,(x,150))   
+    disPlay_surfec.blit(player,(x,150))  
+    disPlay_surfec.blit( star,(random.randint(0, Window_Width), random.randint(0, Window_Height)))
     x+=0.3
     if x>=Window_Width:
         x=100 
