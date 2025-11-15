aed18c194bc7e410d499eb316d279d6cbfb8e562
Wafa Adam <wafaadam548@gmail.com>
2025-11-15 07:47:24 -0500

    import player

--- code/main.py
+++ code/main.py
@@ -5,12 +5,18 @@ disPlay_surfec=pygame.display.set_mode((Window_Width,Window_Height))
 pygame.display.set_caption("Space Shoter")
 running=True
 surf=pygame.Surface((100,200))
+surf.fill("orange")
+x=100
+player=pygame.image.load("images/player.png")
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
     disPlay_surfec.fill("darkGray")    
-    disPlay_surfec.blit(surf,(100,150))    
+    disPlay_surfec.blit(surf,(x,150))   
+    x+=0.3
+    if x>=Window_Width:
+        x=100 
     pygame.display.update()
 pygame.quit()            
 
