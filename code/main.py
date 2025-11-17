25ced5dd584b69da269a20305fb72881df340de6
Wafa Adam <wafaadam548@gmail.com>
2025-11-17 18:00:05 -0500

    add score and correct the code

--- code/main.py
+++ code/main.py
@@ -7,89 +7,97 @@ pygame.mixer.music.load("audio/game_music.wav")
 pygame.mixer.music.play(-1)
 laser_sound = pygame.mixer.Sound("audio/laser.wav")
 
-Window_Width, Window_Height = 1000, 600
-disPlay_surfec = pygame.display.set_mode((Window_Width, Window_Height))
-pygame.display.set_caption("Space Shoter")
-running = True
+w, h = 1000, 600
+screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("Space Shooter")
 
-motors_lest = []
-
-start_positions = [
-    (random.randint(0, Window_Width), random.randint(0, Window_Height))
-    for i in range(20)
-]
-
-motors = pygame.image.load("images/meteor.png").convert_alpha()
 star = pygame.image.load("images/star.png").convert_alpha()
-player = pygame.image.load("images/player.png").convert_alpha()
-player_rect = player.get_frect(center=(Window_Width / 2, 350))
-lasre = pygame.image.load("images/laser.png")
+player_img = pygame.image.load("images/player.png").convert_alpha()
+meteor_img = pygame.image.load("images/meteor.png").convert_alpha()
+laser_img = pygame.image.load("images/laser.png").convert_alpha()
 
-player_direction = pygame.math.Vector2()
-clock = pygame.Clock()
-player_speed =300
-laser_rect = None
+player_rect = player_img.get_rect(center=(w//2, 500))
 
+clock = pygame.time.Clock()
+player_speed = 300
+laser_speed = 350
+meteor_speed = 180
 
-def creat_motors():
-    x_pos = random.randint(0, Window_Width - motors.get_width())
-    motors_rect = motors.get_frect(topleft=(x_pos, -motors.get_height()))
-    motors_lest.append(motors_rect)
+lasers = []
+meteors = []
+spawn_timer = 0
+score = 0
+font = pygame.font.Font(None, 40)
 
-
-spawn_timer = 1
+running = True
 
 while running:
     dt = clock.tick(60) / 1000
 
-    spawn_timer += dt
-    if spawn_timer > 1:
-        creat_motors()
-        spawn_timer = 0
-
-    for motor_rect in motors_lest[:]:
-        motor_rect.y += 150 * dt
-
-        if laser_rect and motor_rect.colliderect(laser_rect):
-            motors_lest.remove(motor_rect)
-            laser_rect = None
-
-        if motor_rect.colliderect(player_rect):
-            running = False
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    move_x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+    move_y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
 
-    if player_direction.length() > 0:
-        player_direction = player_direction.normalize()
+    player_rect.x += move_x * player_speed * dt
+    player_rect.y += move_y * player_speed * dt
+    player_rect.x = max(0, min(player_rect.x, w - player_rect.width))
+    player_rect.y = max(0, min(player_rect.y, h - player_rect.height))
 
-    if keys[pygame.K_SPACE] and laser_rect is None:
-        laser_rect = lasre.get_frect(midbottom=player_rect.midtop)
+    if keys[pygame.K_SPACE]:
+        laser_rect = laser_img.get_rect(midbottom=player_rect.midtop)
+        lasers.append(laser_rect)
         laser_sound.play()
 
-    player_rect.centerx += player_direction.x * player_speed * dt
-    player_rect.centery += player_direction.y * player_speed * dt
+    spawn_timer += dt
+    if spawn_timer > 0.6:
+        x = random.randint(0, w - meteor_img.get_width())
+        m = meteor_img.get_rect(topleft=(x, -50))
+        meteors.append(m)
+        spawn_timer = 0
 
-    disPlay_surfec.fill("darkGray")
+    for l in lasers[:]:
+        l.y -= laser_speed * dt
+        if l.bottom < 0:
+            lasers.remove(l)
 
-    for pos in start_positions:
-        disPlay_surfec.blit(star, pos)
+    for m in meteors[:]:
+        m.y += meteor_speed * dt
+        if m.top > h:
+            meteors.remove(m)
 
-    for motor_rect in motors_lest:
-        disPlay_surfec.blit(motors, motor_rect)
+    for m in meteors[:]:
+        for l in lasers[:]:
+            if m.colliderect(l):
+                meteors.remove(m)
+                lasers.remove(l)
+                score += 1
+                break
 
-    if laser_rect:
-        laser_rect.y -= 300 * dt
-        if laser_rect.bottom < 0:
-            laser_rect = None
-        else:
-            disPlay_surfec.blit(lasre, laser_rect)
+    for m in meteors:
+        if m.colliderect(player_rect):
+            score = 0
+            meteors.clear()
+            lasers.clear()
+            player_rect.center = (w//2, 500)
 
-    disPlay_surfec.blit(player, player_rect)
+    screen.fill("black")
+
+    for _ in range(40):
+        screen.blit(star, (random.randint(0, w), random.randint(0, h)))
+
+    for l in lasers:
+        screen.blit(laser_img, l)
+
+    for m in meteors:
+        screen.blit(meteor_img, m)
+
+    screen.blit(player_img, player_rect)
+
+    score_text = font.render(f"Score: {score}", True, (255,255,255))
+    screen.blit(score_text, (10, 10))
+
     pygame.display.update()
-   
